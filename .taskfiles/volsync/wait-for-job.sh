06212b25aef3b241d59ee754b45dff502b1e38c0
dmcavinue <dan@mcavinue.io>
2023-09-15 16:23:34 -0400

    flip to use contexts

--- .taskfiles/volsync/wait-for-job.sh
+++ .taskfiles/volsync/wait-for-job.sh
@@ -2,11 +2,12 @@
 
 JOB_NAME=$1
 NAMESPACE="${2:-default}"
+CONTEXT=$3
 
 [[ -z "${JOB_NAME}" ]] && echo "Job name not specified" && exit 1
 
 while true; do
-    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
+    STATUS="$(kubectl --context "${JOB_NAME}" -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
     if [ "${STATUS}" == "Pending" ]; then
         break
     fi
