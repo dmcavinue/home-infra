8a5bb76ea62877a5f070e85c3a56c30f4ac574ab
dmcavinue <dan@mcavinue.io>
2023-09-21 18:01:54 -0400

    bump versions

--- .devcontainer/binary-releases.sh
+++ .devcontainer/binary-releases.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
-export TASK_VERSION=v3.27.1
+export TASK_VERSION=v3.30.1
 curl -fsSL "https://github.com/go-task/task/releases/download/${TASK_VERSION}/task_linux_amd64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/task /usr/local/bin/task
 task --version
 
-export CILIUM_VERSION=v0.15.4
+export CILIUM_VERSION=v0.15.8
 curl -fsSL "https://github.com/cilium/cilium-cli/releases/download/${CILIUM_VERSION}/cilium-linux-amd64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/cilium /usr/local/bin/cilium
@@ -16,17 +16,12 @@ curl -fsSL "https://github.com/cilium/hubble/releases/download/${HUBBLE_VERSION}
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/hubble /usr/local/bin/hubble
 
-export TETRA_VERSION="v0.10.0"
+export TETRA_VERSION="v0.11.0"
 curl -fsSL "https://github.com/cilium/tetragon/releases/download/${TETRA_VERSION}/tetra-linux-amd64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/tetra /usr/local/bin/tetra
 
-export GITOPS_VERSION="v0.28.0"
-curl -fsSL "https://github.com/weaveworks/weave-gitops/releases/download/${GITOPS_VERSION}/gitops-Linux-x86_64.tar.gz" \
-    | tar xvz --overwrite --file - --directory /tmp
-mv /tmp/gitops /usr/local/bin/gitops
-
-export GITOPS_VERSION="v0.28.0"
+export GITOPS_VERSION="v0.32.0"
 curl -fsSL "https://github.com/weaveworks/weave-gitops/releases/download/${GITOPS_VERSION}/gitops-Linux-x86_64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/gitops /usr/local/bin/gitops
