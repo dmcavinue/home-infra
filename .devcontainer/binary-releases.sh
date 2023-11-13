0526c57087698d08677235a839a9a284099b823e
dmcavinue <dan@mcavinue.io>
2023-11-13 18:25:01 -0500

    update binaries

--- .devcontainer/binary-releases.sh
+++ .devcontainer/binary-releases.sh
@@ -6,17 +6,17 @@ curl -fsSL "https://github.com/go-task/task/releases/download/${TASK_VERSION}/ta
 mv /tmp/task /usr/local/bin/task
 task --version
 
-export CILIUM_VERSION=v0.15.8
+export CILIUM_VERSION=v0.15.13
 curl -fsSL "https://github.com/cilium/cilium-cli/releases/download/${CILIUM_VERSION}/cilium-linux-amd64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/cilium /usr/local/bin/cilium
 
-export HUBBLE_VERSION="v0.12.0"
+export HUBBLE_VERSION="v0.12.2"
 curl -fsSL "https://github.com/cilium/hubble/releases/download/${HUBBLE_VERSION}/hubble-linux-amd64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/hubble /usr/local/bin/hubble
 
-export TETRA_VERSION="v0.11.0"
+export TETRA_VERSION="v1.0.0"
 curl -fsSL "https://github.com/cilium/tetragon/releases/download/${TETRA_VERSION}/tetra-linux-amd64.tar.gz" \
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/tetra /usr/local/bin/tetra
