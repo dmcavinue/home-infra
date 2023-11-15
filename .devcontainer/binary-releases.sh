54b6d1eb39252b29118d74a6af333a09554d6512
dmcavinue <dan@mcavinue.io>
2023-11-15 17:15:59 -0500

    add i2g

--- .devcontainer/binary-releases.sh
+++ .devcontainer/binary-releases.sh
@@ -41,6 +41,11 @@ curl -fsSL https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_
     | tar xvz --overwrite --file - --directory /tmp
 mv /tmp/k9s /usr/local/bin/k9s
 
+export I2G_VERSION="v0.1.0"
+curl -fsSL https://github.com/kubernetes-sigs/ingress2gateway/releases/download/${I2G_VERSION}/ingress2gateway_Linux_x86_64.tar.gz \
+    | tar xvz --overwrite --file - --directory /tmp
+mv /tmp/ingress2gateway /usr/local/bin/ingress2gateway
+
 curl https://dl.min.io/client/mc/release/linux-amd64/mc \
   -o /usr/local/bin/mc && \
   chmod +x /usr/local/bin/mc
