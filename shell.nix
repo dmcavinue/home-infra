5fc08397902e860537d1f3434d684d84009417da
dmcavinue <dan@mcavinue.io>
2024-08-09 17:40:07 -0400

    add basic nix config

---
+++ shell.nix
@@ -0,0 +1,31 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config.allowUnfree = true; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    ansible
+    fluxcd
+    git
+    go-task
+    helm
+    jq
+    k9s
+    kubectl
+    python3
+    vault
+    yq
+  ];
+
+  EDITOR = "nano";
+  LANGUAGE = "C.UTF-8";
+  LANG="C.UTF-8";
+  LC_COLLATE="C.UTF-8";
+  LC_CTYPE="C.UTF-8";
+  LC_MONETARY="C.UTF-8";
+  LC_NUMERIC="C.UTF-8";
+  LC_TIME="C.UTF-8";
+  LC_MESSAGES="C.UTF-8";
+  LC_ALL="C.UTF-8";
+}
